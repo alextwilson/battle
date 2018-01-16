bf9fd555d58be32ed92fc89ff03557830276e29d
alextwilson <github@alextwilson.co.uk>
2018-01-16 18:52:31 +0000

    Refactored test and input in form to use clearer 'click_button'

--- spec/features/form_spec.rb
+++ spec/features/form_spec.rb
@@ -3,7 +3,7 @@ feature 'form used to take player names' do
     visit('/')
     fill_in 'player1', with: 'Alex'
     fill_in 'player2', with: 'Ben'
-    find('input[name="submit"]').click
+    click_button "Submit"
 
     expect(page).to have_content('Alex' && 'Ben')
   end
