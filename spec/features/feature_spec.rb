28a923d8aebabebbd22dee71fd2b6d47944c4c2a
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-01-16 17:21:23 +0000

    Updated the rspec description

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,5 +1,5 @@
 feature 'Capybara' do
-  scenario "Should check content of homepage" do
+  scenario "Testing infrastructure working!" do
     visit('/')
     expect(page).to have_content('Hello Battle!')
   end
