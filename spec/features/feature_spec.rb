baef05bb31c81d950a1e3675535ff3ff0bdca9a6
alextwilson <github@alextwilson.co.uk>
2018-01-16 18:42:13 +0000

    Form to submit player names

--- spec/features/feature_spec.rb
+++
@@ -1,6 +0,0 @@
-feature 'Capybara' do
-  scenario "Testing infrastructure working!" do
-    visit('/')
-    expect(page).to have_content('Hello Battle!')
-  end
-end
---
+++ spec/features/form_spec.rb
@@ -0,0 +1,10 @@
+feature 'form used to take player names' do
+  scenario 'displays names' do
+    visit('/')
+    fill_in 'player1', with: 'Alex'
+    fill_in 'player2', with: 'Ben'
+    find('input[name="submit"]').click
+
+    expect(page).to have_content('Alex' && 'Ben')
+  end
+end
