1cbd4986a52ef1f8b3a38a67a5c58e0e95c7fca7
alextwilson <github@alextwilson.co.uk>
2018-01-17 14:53:05 +0000

    Implement session, moved rendering view to own route

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    @player1name = params[:player1]
-    @player2name = params[:player2]
-    erb :play
+    session[:player1] = params[:player1]
+    session[:player2] = params[:player2]
+    redirect '/play'
   end
 
+  get '/play' do
+    @player1name = session[:player1]
+    @player2name = session[:player2]
+    erb :play
+  end
 end
